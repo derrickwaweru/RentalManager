13d74a0a74f9053743129e1f6be79192b77117f0
Musab36 <musabhd@gmail.com>
2017-10-12 14:58:20 +0300

    AddOwnerActivity ui design

---
+++ app/src/main/java/com/example/derrick/rentalmanager/AddOwnerActivity.java
@@ -0,0 +1,13 @@
+package com.example.derrick.rentalmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddOwnerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_owner);
+    }
+}
--- app/src/main/java/com/example/derrick/rentalmanager/MainActivity.java
+++ app/src/main/java/com/example/derrick/rentalmanager/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.derrick.rentalmanager;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.ownerImageView) ImageView mOwnerImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mOwnerImageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mOwnerImageView) {
+            Intent intent = new Intent(MainActivity.this, AddOwnerActivity.class);
+            startActivity(intent);
+        }
     }
 }
