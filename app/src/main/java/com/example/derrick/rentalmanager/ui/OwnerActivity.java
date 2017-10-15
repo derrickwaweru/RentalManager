fba47b81ad73ac5695462d75af5d193e340d03d4
Musab36 <musabhd@gmail.com>
2017-10-15 16:31:29 +0300

    Owner specialities

--- app/src/main/java/com/example/derrick/rentalmanager/ui/OwnerActivity.java
+++ app/src/main/java/com/example/derrick/rentalmanager/ui/OwnerActivity.java
@@ -3,16 +3,39 @@ package com.example.derrick.rentalmanager.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.derrick.rentalmanager.R;
 
-public class OwnerActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class OwnerActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.ownerReportsImageView) ImageView mOwnerReports;
+    @Bind(R.id.ownerTenantsImageView) ImageView mOwnerTenants;
+    @Bind(R.id.ownerPropertyImageView) ImageView mOwnerProperty;
+    @Bind(R.id.ownerPaymentsImageView) ImageView mOwnerPayments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
+
+        mOwnerReports.setOnClickListener(this);
+        mOwnerTenants.setOnClickListener(this);
+        mOwnerProperty.setOnClickListener(this);
+        mOwnerPayments.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mOwnerReports) {
+            Intent intent = new Intent(OwnerActivity.this, ReportsActivity.class);
+            startActivity(intent);
+        }
     }
 }
