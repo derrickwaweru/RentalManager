c6671fa9a12c3cfdb6d3a6dcb5731c3f4d8b0970
Musab36 <musabhd@gmail.com>
2017-10-15 18:51:22 +0300

    Manager specialities

--- app/src/main/java/com/example/derrick/rentalmanager/ui/ManagerActivity.java
+++ app/src/main/java/com/example/derrick/rentalmanager/ui/ManagerActivity.java
@@ -1,18 +1,38 @@
 package com.example.derrick.rentalmanager.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.derrick.rentalmanager.R;
 
-public class ManagerActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ManagerActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.managerReportsImageView) ImageView mManagerReports;
+    @Bind(R.id.managerTenants) ImageView mManagerTenants;
+    @Bind(R.id.workersImageView) ImageView mWorkersImageView;
+    @Bind(R.id.managerPayments) ImageView mManagerPayments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
+
+        mManagerReports.setOnClickListener(this);
+    }
+
+    @Override
+    public void  onClick(View v) {
+        if(v == mManagerReports) {
+            Intent intent = new Intent(ManagerActivity.this, ReportsActivity.class);
+            startActivity(intent);
+        }
     }
 }
